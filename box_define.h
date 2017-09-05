8b7ee547f749b3e6a829ea2bb04de6d47245ab15
mayudong <mayudong@kingsoft.com>
2017-09-05 20:47:31 +0800

    box define

---
+++ box_define.h
@@ -0,0 +1,32 @@
+#ifndef _BOX_DEFINE__
+#define _BOX_DEFINE__
+
+#include <stdint.h>
+
+typedef struct BaseBox
+{
+	uint32_t size;
+	uint32_t type;
+	struct BaseBox* next;
+}BaseBox;
+
+typedef struct FullBox
+{
+	BaseBox header;
+	uint8_t version;
+	uint32_t flags;
+	struct BaseBox* next;
+}FullBox;
+
+#define MAX_COMPATIBLE_BRANDS_COUNT 16
+typedef struct FileTypeBox
+{
+	BaseBox header;
+	uint32_t major_brand;
+	uint32_t minor_version;
+	uint32_t compatible_brands[MAX_COMPATIBLE_BRANDS_COUNT];
+}FileTypeBox;
+
+
+
+#endif
